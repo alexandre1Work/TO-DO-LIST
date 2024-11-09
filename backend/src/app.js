2de470a3b761a08488620eafab2df8fb5449dd25
alexandre1Work <alexandre.velosowork@gmail.com>
2024-11-08 23:32:16 -0300

    Adicionando o cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 //importanto o express 
 const express = require('express');
+//permite que outros sites tenham conexão com essa API
+const cors = require('cors');
 //importando o arquivo de rotas
 const router = require('./router');
 
@@ -7,7 +9,7 @@ const router = require('./router');
 const app = express();
 //para conseguir usar arquivor json
 app.use(express.json());
-
+app.use(cors());
 //sempre vai usar as rotas
 app.use(router);
 
